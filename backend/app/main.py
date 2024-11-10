09be8508ef5581625d801dc4a3967424326543f1
StrategoEstudio <strategoestudio@gmail.com>
2024-11-10 08:16:48 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,23 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import chat
 
 app = FastAPI()
 
+# Configurar CORS para aceptar solicitudes desde el frontend en Vercel (o cualquier otro frontend)
+frontend_url = os.getenv("FRONTEND_URL", "http://localhost:3000")  # Cambia el valor predeterminado según tus necesidades
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Cambia el origen según tu frontend
+    allow_origins=[frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Incluye el router de chat
+# Incluir el router de chat
 app.include_router(chat.router)
 
 @app.get("/")
 async def root():
-    return {"message": "API está funcionando correctamente"}
+    return {"message": "API funcionando correctamente en Railway"}
